1258369f6ffe967b6df1504d7ae0e2fd27ea092e
Joseph Constantakis <jcon5294@gmail.com>
2014-12-27 21:32:05 -0500

    create project from cocoa touch framework template

---
+++ Vlow/Vlow/Vlow.h
@@ -0,0 +1,17 @@
+//
+//  Vlow.h
+//  Vlow
+//
+//  Created by Joseph Constantakis on 12/27/14.
+//
+//
+
+//! Project version number for Vlow.
+FOUNDATION_EXPORT double VlowVersionNumber;
+
+//! Project version string for Vlow.
+FOUNDATION_EXPORT const unsigned char VlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vlow/PublicHeader.h>
+
+
